e53b645a3e195ccac0bfbb48cd1ec2507ff74055
Ayush Kumar <ayushtiwari0803@gmail.com>
2025-03-22 12:48:09 +0530

    Debugging deployement error

--- src/app.js
+++ src/app.js
@@ -1,39 +1,45 @@
 const express = require("express");
-require('dotenv').config();
-const cookieParser = require('cookie-parser');
-const jwt = require('jsonwebtoken');
+require("dotenv").config();
+const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
 
 const connectDB = require("./config/database");
 const User = require("./models/user");
-const PORT = process.env.PORT
-const privateKey = process.env.PRIVATE_KEY
+const PORT = process.env.PORT;
+const privateKey = process.env.PRIVATE_KEY;
 const app = express();
-const cors = require("cors")
+const cors = require("cors");
 
-app.use(cors({
-    origin : "http://localhost:5173",
-    credentials : true,
-}))
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 
-const authRouter = require('./routes/auth');
-const profileRouter = require('./routes/profile')
-const requestRouter = require('./routes/request')
-const userRouter = require('./routes/user')
+const authRouter = require("./routes/auth");
+const profileRouter = require("./routes/profile");
+const requestRouter = require("./routes/request");
+const userRouter = require("./routes/user");
 
-app.use('/', authRouter);
-app.use('/', profileRouter);
-app.use('/api', requestRouter);
-app.use('/', userRouter);
+app.use("/", authRouter);
+app.use("/", profileRouter);
+app.use("/api", requestRouter);
+app.use("/", userRouter);
+
+app.get("/", (req, res) => {
+  res.send("Server is running!");
+});
 
 connectDB()
-    .then(() => {
-        console.log("Database Connection established...");
-        app.listen(PORT, () => {
-            console.log("Server listening successfully on port", PORT + "...");
-        });
-    })
-    .catch((err) => {
-        console.error("Database cannot be connected");
+  .then(() => {
+    console.log("Database Connection established...");
+    app.listen(PORT, () => {
+      console.log("Server listening successfully on port", PORT + "...");
     });
+  })
+  .catch((err) => {
+    console.error("Database cannot be connected");
+  });
